b3cb29a86df2fc64236f26e40d08214278f02993
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-09-27 03:49:30 -0400

    Log values

--- Program.cs
+++ Program.cs
@@ -352,6 +352,7 @@ public class Program
         var initialPrice = (int)priceOverview["initial"];
         var currentPrice = (int)priceOverview["final"];
         var discountPercent = (int)priceOverview["discount_percent"];
+        Console.WriteLine($"{appid}: {{initial: {initialPrice}, current: {currentPrice}, discountPercent: {discountPercent}}}");
 
         var fileName = $"{appid}_price.json";
 
